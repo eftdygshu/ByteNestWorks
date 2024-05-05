e6ebfc2f910923cb28a594088afa6974b0c81b63
eftdygshu <isjckbalbulut09@hotmail.com>
2024-05-05 23:02:48 +0700

    commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function minMeetingRooms(intervals) {
-  intervals.sort((a, b) => a[0] - b[0]);
-  const minHeap = new MinHeap();
-  for (const interval of intervals) {
-    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
-      minHeap.pop();
-    }
-    minHeap.push(interval[1]);
+function findMaxConsecutiveOnes(nums) {
+  let maxCount = 0;
+  let count = 0;
+  for (const num of nums) {
+    if (num === 1) {
+      count++;
+      maxCount = Math.max(maxCount, count);
+    } else count = 0;
   }
-  return minHeap.size();
+  return maxCount;
 }
